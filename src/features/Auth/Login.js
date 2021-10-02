42ff9ad36620b7ae8cfe768c1f64f0779dd81b32
GaneshMani <ganeshmani009@gmail.com>
2021-10-02 17:24:24 +0530

    changes

--- src/features/Auth/Login.js
+++ src/features/Auth/Login.js
@@ -74,6 +74,7 @@ const LoginComponent = () => {
                                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 />
                             </div>
+                            <p class="mt-2 text-sm text-gray-500" id="email-description">Hint: admin@gmail.com</p>
                         </div>
 
                         <div>
@@ -91,6 +92,7 @@ const LoginComponent = () => {
                                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 />
                             </div>
+                            <p class="mt-2 text-sm text-gray-500" id="email-description">Hint: admin</p>
                         </div>
 
                         <div>
